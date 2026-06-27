ecaf26845af878a597932750571c85996b1b665c
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-06-27 19:33:12 +0530

    Add LeetCode/Medium/50-powx-n/try-1.cpp

---
+++ LeetCode/Medium/50-powx-n/try-1.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem #50: Pow(x, n)
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 4/3/2025, 4:36:00 PM
+ * Link: https://leetcode.com/problems/powx-n/
+ */
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);
+    }
+};
