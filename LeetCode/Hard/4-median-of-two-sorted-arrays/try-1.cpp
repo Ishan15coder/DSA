3a9e0eed9dbf732a249be5f0335df8a0916d854d
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-06-27 19:28:40 +0530

    Add LeetCode/Hard/4-median-of-two-sorted-arrays/try-1.cpp

---
+++ LeetCode/Hard/4-median-of-two-sorted-arrays/try-1.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem #4: Median of Two Sorted Arrays
+ * Difficulty: Hard
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/22/2025, 12:33:22 PM
+ * Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+ */
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        for(int i=0;i<nums2.size();i++){
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());
+        int n=nums1.size();
+        if(nums1.size()%2!=0){
+            return double(nums1[n / 2]);
+        }
+        
+            return (nums1[n / 2 - 1] + nums1[n / 2]) / 2.0;
+        
+    }
+};
