a621043a8358cf4cda9d76e49048a47b5c465cdd
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-06-27 19:34:27 +0530

    Add LeetCode/Easy/69-sqrtx/try-1.cpp

---
+++ LeetCode/Easy/69-sqrtx/try-1.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem #69: Sqrt(x)
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 2/18/2025, 10:31:30 PM
+ * Link: https://leetcode.com/problems/sqrtx/
+ */
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int p=sqrt(x);
+        return p;
+    }
+};
