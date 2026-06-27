1b904ddfdae5b0720027b3732da017bace6b752a
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-06-27 19:34:24 +0530

    Add LeetCode/Easy/2235-add-two-integers/try-1.cpp

---
+++ LeetCode/Easy/2235-add-two-integers/try-1.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem #2235: Add Two Integers
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 2/18/2025, 10:39:24 PM
+ * Link: https://leetcode.com/problems/add-two-integers/
+ */
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
