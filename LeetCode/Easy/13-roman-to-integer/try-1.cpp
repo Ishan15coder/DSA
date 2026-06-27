776ed36191006949a9b82b0f3211af52c8a55cfe
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-06-27 19:35:05 +0530

    Add LeetCode/Easy/13-roman-to-integer/try-1.cpp

---
+++ LeetCode/Easy/13-roman-to-integer/try-1.cpp
@@ -0,0 +1,70 @@
+/*
+ * Problem #13: Roman to Integer
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 11/1/2024, 1:04:55 AM
+ * Link: https://leetcode.com/problems/roman-to-integer/
+ */
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n=s.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='I'){
+                 if(s[i+1]=='V'){
+                    sum+=4;
+                    i=i+1;
+                 }
+                 else if(s[i+1]=='X'){
+                    sum+=9;
+                    i=i+1;
+                 }
+                 else{
+                sum+=1;
+                 }
+            }
+            else if(s[i]=='V'){
+                sum+=5;
+            }
+            else if(s[i]=='X'){
+                 if(s[i+1]=='L'){
+                    sum+=40;
+                    i=i+1;
+                 }
+                 else if(s[i+1]=='C'){
+                    sum+=90;
+                    i=i+1;
+                 }
+                 else{
+                sum+=10;
+                 }
+            }
+            else if(s[i]=='L'){
+                sum+=50;
+            }
+            else if(s[i]=='C'){
+                if(s[i+1]=='D'){
+                    sum+=400;
+                    i=i+1;
+                 }
+                 else if(s[i+1]=='M'){
+                    sum+=900;
+                    i=i+1;
+                 }
+                 else{
+                sum+=100;
+                 }
+            }
+            else if(s[i]=='D'){
+                sum+=500;
+            }else if(s[i]=='M'){
+                sum+=1000;
+            }
+        }
+        return sum;
+    }
+};
