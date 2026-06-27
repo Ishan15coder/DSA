582d2b1cd7d76304bde0dc9fd8263fac0a40cb01
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-06-27 19:30:17 +0530

    Add LeetCode/Easy/2942-find-words-containing-character/try-2.cpp

---
+++ LeetCode/Easy/2942-find-words-containing-character/try-2.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem #2942: Find Words Containing Character
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 5/24/2025, 6:11:23 PM
+ * Link: https://leetcode.com/problems/find-words-containing-character/
+ */
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        int k=words.size();
+        vector<int>ans;
+        for(int i=0;i<k;i++){
+            int n=words[i].length();
+            for(int j=0;j<n;j++){
+                if(words[i][j]==x){
+                    ans.push_back(i);
+                  break;
+                }
+            }
+        }
+        return ans;
+    }
+};
