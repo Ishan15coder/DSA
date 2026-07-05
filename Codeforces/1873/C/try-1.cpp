e8b903f63de57671f9b14606c4a63d094c5c640c
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:04:10 +0530

    Add Codeforces/1873/C/try-1.cpp

---
+++ Codeforces/1873/C/try-1.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem : Target Practice
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/8/2025, 11:51:24 AM
+ * Link: https://codeforces.com/contest/1873/problem/C
+ */
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t>0){
+        int points=0;
+        for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            char p;
+            cin>>p;
+            if(p=='X'){
+               points+=min({i,j,9-i,9-j})+1;
+            }
+        }
+    }
+        cout<<points<<endl;
+        t--;
+    }
+
+
+    return 0;
+}
