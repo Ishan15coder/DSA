f9bd9d27c0cdb2b844c6bb2049a9be8d5cd952b4
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:03:04 +0530

    Add Codeforces/1834/A/try-1.cpp

---
+++ Codeforces/1834/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Unit Array
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/16/2025, 12:02:52 PM
+ * Link: https://codeforces.com/contest/1834/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
