80c78ec61432567d7005887eb44536a697008476
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:05:19 +0530

    Add Codeforces/4/A/try-1.cpp

---
+++ Codeforces/4/A/try-1.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem : Watermelon
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 4/4/2025, 1:48:35 AM
+ * Link: https://codeforces.com/contest/4/problem/A
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0&&n>2){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
