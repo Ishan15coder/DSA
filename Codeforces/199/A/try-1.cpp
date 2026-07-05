e4e255b7ed76d0626da7fcca71de02e2eb6df92a
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:04:29 +0530

    Add Codeforces/199/A/try-1.cpp

---
+++ Codeforces/199/A/try-1.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem : Hexadecimal's theorem
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/1/2025, 8:45:13 PM
+ * Link: https://codeforces.com/contest/199/problem/A
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<0<<" "<<0<<" "<<n;
+    
+
+    return 0;
+}
