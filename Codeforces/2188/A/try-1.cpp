e59bbf156d489869c4e31031ba4de4e74c3a6477
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:57:33 +0530

    Add Codeforces/2188/A/try-1.cpp

---
+++ Codeforces/2188/A/try-1.cpp
@@ -0,0 +1,38 @@
+/*
+ * Problem : Divisible Permutation
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 2/5/2026, 11:56:52 PM
+ * Link: https://codeforces.com/contest/2188/problem/A
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int mid;
+        if(n%2 == 0){
+            mid=n/2 - 1;
+        }else{
+            mid=n/2;
+        }
+        
+        for(int i=mid;i>=0;i--){
+            if(i == mid && n%2 == 1){
+                cout<<i+1<<" ";
+            }
+            else{
+                cout<<i+1<<" "<<n-i<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
