0a1225b77eb3a6604d0db395b4a576b84a963f10
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:04:16 +0530

    Add Codeforces/1878/A/try-1.cpp

---
+++ Codeforces/1878/A/try-1.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem : How Much Does Daytona Cost?
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/6/2025, 10:40:41 PM
+ * Link: https://codeforces.com/contest/1878/problem/A
+ */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t>0){
+        int n,k;
+        cin>>n>>k;
+        vector<int>p(n);
+        for(int i=0;i<n;i++){
+            cin>>p[i];
+        }
+        int ch=0;
+        for(int i=0;i<n;i++){
+            if(p[i]==k)ch=1;
+        }
+        if(ch)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+        t--;
+    }
+
+
+    return 0;
+}
