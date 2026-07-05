9b0df5c999e3ed2e91c654a49935521d1d867c92
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:05:07 +0530

    Add Codeforces/236/A/try-1.cpp

---
+++ Codeforces/236/A/try-1.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem : Boy or Girl
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 5/29/2025, 10:53:26 AM
+ * Link: https://codeforces.com/contest/236/problem/A
+ */
+
+#include<iostream>
+#include<cmath>
+#include<set>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    set<char>p;
+    for(int i=0;i<s.size();i++){
+        p.insert(s[i]);
+    }
+    if(p.size()%2!=0){
+        cout<<"IGNORE HIM!";
+    }
+    else{
+        cout<<"CHAT WITH HER!";
+    }
+
+    return 0;
+}
