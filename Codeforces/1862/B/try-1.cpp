9da93945ca9edf9839c37404ea229ab6f70ac596
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:03:47 +0530

    Add Codeforces/1862/B/try-1.cpp

---
+++ Codeforces/1862/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Sequence Game
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/9/2025, 11:43:36 AM
+ * Link: https://codeforces.com/contest/1862/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
