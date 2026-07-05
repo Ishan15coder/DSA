ab7ed4a24d77dc2e6adb44096f2800d1767d7e9b
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:57:18 +0530

    Add Codeforces/579/A/try-1.cpp

---
+++ Codeforces/579/A/try-1.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem : Raising Bacteria
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 2/14/2026, 2:07:44 PM
+ * Link: https://codeforces.com/contest/579/problem/A
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    long long x;
+    cin>>x;
+    cout<<__builtin_popcount(x);
+    
+    
+    
+    return 0;
+}
