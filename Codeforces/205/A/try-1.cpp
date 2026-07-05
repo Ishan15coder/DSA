9017df58122c159f018c35771f59c0e7adfd3613
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:04:28 +0530

    Add Codeforces/205/A/try-1.cpp

---
+++ Codeforces/205/A/try-1.cpp
@@ -0,0 +1,41 @@
+/*
+ * Problem : Little Elephant and Rozdil
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 6/29/2025, 11:19:25 AM
+ * Link: https://codeforces.com/contest/205/problem/A
+ */
+
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>b(n);
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+    }
+    int min=INT_MAX;
+    int mnc=0;
+    int ans=0;
+    for(int i=0;i<n;i++){
+        if(b[i]<min){
+            ans=i+1;
+            min=b[i];
+            mnc=1;
+            continue;
+        }
+        if(b[i]==min){
+            mnc++;
+        }
+    }
+    if(mnc>1)cout<<"Still Rozdil";
+    else{
+        cout<<ans;
+    }
+    return 0;
+}
