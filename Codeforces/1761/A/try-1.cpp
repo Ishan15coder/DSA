91c185a1702fe7e8b9fa670374102aee1b07f7d8
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:00:40 +0530

    Add Codeforces/1761/A/try-1.cpp

---
+++ Codeforces/1761/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Two Permutations
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/24/2025, 9:52:14 PM
+ * Link: https://codeforces.com/contest/1761/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
