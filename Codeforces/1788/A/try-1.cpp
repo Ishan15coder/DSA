9d41b80c0253595d6c5fafb2e80bfab533f95b87
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:01:03 +0530

    Add Codeforces/1788/A/try-1.cpp

---
+++ Codeforces/1788/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : One and Two
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/22/2025, 9:31:16 PM
+ * Link: https://codeforces.com/contest/1788/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
