aad1ee2c42a9e047b61b0ae5915f865de384efc0
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:01:25 +0530

    Add Codeforces/1791/C/try-1.cpp

---
+++ Codeforces/1791/C/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Prepend and Append
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/20/2025, 9:10:47 PM
+ * Link: https://codeforces.com/contest/1791/problem/C
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
