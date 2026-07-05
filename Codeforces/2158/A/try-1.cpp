ba0d6739109da17a9b5426c0251f82811584bc07
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:58:26 +0530

    Add Codeforces/2158/A/try-1.cpp

---
+++ Codeforces/2158/A/try-1.cpp
@@ -0,0 +1,32 @@
+/*
+ * Problem : Suspension
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 11/29/2025, 8:14:57 PM
+ * Link: https://codeforces.com/contest/2158/problem/A
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int y,r;
+        cin>>y>>r;
+        int nv=r;
+        if(y>=2){
+            nv+=floor(y/2);
+        }
+        if(nv>n)nv=n;
+        cout<<nv<<endl;
+    }
+
+
+
+    return 0;
+}
