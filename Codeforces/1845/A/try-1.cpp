4bf315b2ff5333e41fc4131c34a6eac13415b170
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:03:34 +0530

    Add Codeforces/1845/A/try-1.cpp

---
+++ Codeforces/1845/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Forbidden Integer
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/16/2025, 10:58:53 AM
+ * Link: https://codeforces.com/contest/1845/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
