d87dc6bfadeac8c43b403cde30c462c607aae8df
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:58:21 +0530

    Add Codeforces/1537/B/try-1.cpp

---
+++ Codeforces/1537/B/try-1.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem : Bad Boy
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 12/29/2025, 1:22:00 AM
+ * Link: https://codeforces.com/contest/1537/problem/B
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,a,b;
+        cin>>a>>b>>x>>y;
+        
+        cout<<1<<" "<<1<<" "<<a<<" "<<b<<endl;
+        
+
+        }
+
+
+
+
+
+    return 0;
+}
