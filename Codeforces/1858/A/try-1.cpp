1810ddef22d39f1caa3e7a4695de31e9aa884ada
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:03:56 +0530

    Add Codeforces/1858/A/try-1.cpp

---
+++ Codeforces/1858/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Buttons
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/11/2025, 11:59:21 AM
+ * Link: https://codeforces.com/contest/1858/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
