2fc3b73f1af74c3cb7f74759536810f65142fa1c
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:02:46 +0530

    Add Codeforces/1831/A/try-1.cpp

---
+++ Codeforces/1831/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Twin Permutations
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/16/2025, 10:11:47 PM
+ * Link: https://codeforces.com/contest/1831/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
