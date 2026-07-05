e4bf86412dc825738b5c90f32b84bf309160fe5b
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:58:32 +0530

    Add Codeforces/2167/B/try-1.cpp

---
+++ Codeforces/2167/B/try-1.cpp
@@ -0,0 +1,39 @@
+/*
+ * Problem : Your Name
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 10/28/2025, 8:17:10 PM
+ * Link: https://codeforces.com/contest/2167/problem/B
+ */
+
+#include<iostream>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s,to;
+        cin>>s>>to;
+        // unordered_map<char,int>a;
+        // unordered_map<char,int>b;
+        // for(int i=0;i<s.length();i++){
+        //     a[s[i]]++;
+        // }
+        // for(int i=0;i<to.length();i++){
+        //     b[to[i]]++;
+        // }
+        sort(s.begin(),s.end());
+        sort(to.begin(),to.end());
+         if(s==to)cout<<"YES"<<endl;
+         else cout<<"NO"<<endl;
+    }
+
+
+    return 0;
+}
