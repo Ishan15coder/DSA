d4bf4159e564c06edf1be79573e3ea9847d73cde
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:57:28 +0530

    Add Codeforces/2188/C/try-1.cpp

---
+++ Codeforces/2188/C/try-1.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem : Restricted Sorting
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 2/6/2026, 6:23:57 PM
+ * Link: https://codeforces.com/contest/2188/problem/C
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        vector<long long>a(n);
+        for(long long i=0;i<n;i++){
+            cin>>a[i];
+        }
+        long long ans=INT_MAX;
+        vector<long long>b=a;
+        sort(b.begin(),b.end());
+        for(long long i=0;i<n;i++){
+                if(a[i]!=b[i]){
+                    long long v=max(a[i]-b[0],b[n-1]-a[i]);
+                    ans=min(ans,v);
+            }
+        }
+        if(ans==INT_MAX)ans=-1;
+        cout<<ans<<endl;
+    }
+ 
+}
