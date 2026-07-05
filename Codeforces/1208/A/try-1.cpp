68382b4ba936ec23268a9b97277b37103c590d27
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:59:33 +0530

    Add Codeforces/1208/A/try-1.cpp

---
+++ Codeforces/1208/A/try-1.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem : XORinacci
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/9/2025, 2:23:54 AM
+ * Link: https://codeforces.com/contest/1208/problem/A
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b;
+        long long n;
+        cin >> a >> b >> n;
+
+        if(n % 3 == 0) cout << a << "\n";
+        else if(n % 3 == 1) cout << b << "\n";
+        else cout << (a ^ b) << "\n";
+    }
+    return 0;
+}
