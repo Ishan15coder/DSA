82eb2152c903b7dbca3ef0db1169795841973324
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:01:33 +0530

    Add Codeforces/1789/A/try-1.cpp

---
+++ Codeforces/1789/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Serval and Mocha's Array
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/21/2025, 2:03:49 PM
+ * Link: https://codeforces.com/contest/1789/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
