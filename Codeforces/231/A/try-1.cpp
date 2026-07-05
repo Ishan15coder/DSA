8fdffa12d1304c863437475e0b2a46e57ea28220
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:04:48 +0530

    Add Codeforces/231/A/try-1.cpp

---
+++ Codeforces/231/A/try-1.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem : Team
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 6/6/2025, 11:29:03 AM
+ * Link: https://codeforces.com/contest/231/problem/A
+ */
+
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cin>>n;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b+c>=2){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
