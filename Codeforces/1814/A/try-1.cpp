11a76dc2e2c0410bf2cfa3ae8294a51e6f0d0c1d
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:02:17 +0530

    Add Codeforces/1814/A/try-1.cpp

---
+++ Codeforces/1814/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Coins
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/16/2025, 10:53:21 PM
+ * Link: https://codeforces.com/contest/1814/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
