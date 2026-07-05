86e78858515fb43fd94677ee02f90a89aad060d4
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:57:56 +0530

    Add Codeforces/2185/A/try-1.cpp

---
+++ Codeforces/2185/A/try-1.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem : Perfect Root
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 1/18/2026, 8:19:16 PM
+ * Link: https://codeforces.com/contest/2185/problem/A
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++){
+            cout<<i*i<<" ";
+        }
+        cout<<endl;
+    }
+}
