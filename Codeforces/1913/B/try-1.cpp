039d29a86365ff60a5428a1c4dfd0667a8c68296
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:58:06 +0530

    Add Codeforces/1913/B/try-1.cpp

---
+++ Codeforces/1913/B/try-1.cpp
@@ -0,0 +1,50 @@
+/*
+ * Problem : Swap and Delete
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 1/1/2026, 9:35:27 PM
+ * Link: https://codeforces.com/contest/1913/problem/B
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int z=0,o=0;
+        int n=s.length();
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1')o++;
+            else z++;
+        }
+        int lt=0;
+        for(int i=0;i<n;i++){
+
+            if(s[i]=='0'&&o>0){
+                o--;
+                lt++;
+            }
+            else if(s[i]=='1'&&z>0){
+                z--;
+                lt++;
+            }
+            else{
+                break;
+            }
+        }
+        cout<<s.length()-lt<<endl;
+
+
+
+    }
+
+
+
+
+    return 0;
+}
