53cd9d9928da9e16f5f24384a5912639481677da
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:01:47 +0530

    Add Codeforces/1806/A/try-1.cpp

---
+++ Codeforces/1806/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Walking Master
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/19/2025, 8:47:26 PM
+ * Link: https://codeforces.com/contest/1806/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
