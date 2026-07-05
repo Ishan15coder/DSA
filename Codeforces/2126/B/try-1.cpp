5a76003204568b3ddba3247e17154d5a84cce796
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:02:04 +0530

    Add Codeforces/2126/B/try-1.cpp

---
+++ Codeforces/2126/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : No Casino in the Mountains
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/17/2025, 8:23:35 PM
+ * Link: https://codeforces.com/contest/2126/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
