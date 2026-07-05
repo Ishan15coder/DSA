5a3ce42a655f4c0a9eb878f33e8868413493a0a4
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:03:17 +0530

    Add Codeforces/1857/A/try-1.cpp

---
+++ Codeforces/1857/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Array Coloring
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/15/2025, 10:46:09 AM
+ * Link: https://codeforces.com/contest/1857/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
