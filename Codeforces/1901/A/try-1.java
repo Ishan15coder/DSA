a46b1d512bb8a947b96c50353eb0272e05544dfe
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:00:31 +0530

    Add Codeforces/1901/A/try-1.java

---
+++ Codeforces/1901/A/try-1.java
@@ -0,0 +1,12 @@
+/*
+ * Problem : Line Trip
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Java 21
+ * Date: 7/24/2025, 9:54:59 PM
+ * Link: https://codeforces.com/contest/1901/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
