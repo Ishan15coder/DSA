e7a5c9563de59ca2c81454024fcb2fd44553b948
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:01:12 +0530

    Add Codeforces/1783/A/try-1.cpp

---
+++ Codeforces/1783/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Make it Beautiful
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/22/2025, 9:51:52 PM
+ * Link: https://codeforces.com/contest/1783/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
