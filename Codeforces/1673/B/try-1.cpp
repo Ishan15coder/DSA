d9cbb23dfd074fc0d7cc2d75adf3eccb286e583d
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:56:28 +0530

    Add Codeforces/1673/B/try-1.cpp

---
+++ Codeforces/1673/B/try-1.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem : A Perfectly Balanced String?
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 4/6/2026, 7:33:58 PM
+ * Link: https://codeforces.com/contest/1673/problem/B
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int n = s.size();
+        set<char> st;
+        int k = 0;
+
+       
+        for (; k < n; k++) {
+            if (st.count(s[k])) break;
+            st.insert(s[k]);
+        }
+
+        bool ok = true;
+
+        for (int i = k; i < n; i++) {
+            if (s[i] != s[i - k]) {
+                ok = false;
+                break;
+            }
+        }
+
+        cout << (ok ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
