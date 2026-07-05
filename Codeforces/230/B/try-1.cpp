93c79ec9f51f7edbd6f9905d3eae2e310bc710ad
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 09:59:44 +0530

    Add Codeforces/230/B/try-1.cpp

---
+++ Codeforces/230/B/try-1.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem : T-primes
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/3/2025, 1:34:46 PM
+ * Link: https://codeforces.com/contest/230/problem/B
+ */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+bool isPrime(long long n) {
+    if (n < 2) return false;
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        long long root = sqrtl(a[i]);   // long double sqrt
+        if (root * root == a[i] && isPrime(root)) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
