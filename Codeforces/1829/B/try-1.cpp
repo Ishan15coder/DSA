f50646d1c9e998883feaee2471db004836c681e8
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:02:25 +0530

    Add Codeforces/1829/B/try-1.cpp

---
+++ Codeforces/1829/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Blank Space
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/16/2025, 10:40:36 PM
+ * Link: https://codeforces.com/contest/1829/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
