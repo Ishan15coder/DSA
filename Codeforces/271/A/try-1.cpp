8efa9007a3fd399d7c7c10919bcc4283dfa6fc8e
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:05:00 +0530

    Add Codeforces/271/A/try-1.cpp

---
+++ Codeforces/271/A/try-1.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem : Beautiful Year
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 5/29/2025, 2:12:31 PM
+ * Link: https://codeforces.com/contest/271/problem/A
+ */
+
+#include<iostream>
+#include<cmath>
+#include<set>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    set<int>d;
+    while(1){
+        n++;
+        int j=n;
+        while(j>0){
+            d.insert(j%10);
+            j=j/10;
+        }
+        if(d.size()==4) break;
+        d.clear();
+    }
+    cout<<n;
+    return 0;
+}
