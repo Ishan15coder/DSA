98cfdcd024758cc52f07778b2f64e1c9134d5094
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:01:56 +0530

    Add Codeforces/2126/C/try-1.cpp

---
+++ Codeforces/2126/C/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : I Will Definitely Make It
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/17/2025, 9:21:49 PM
+ * Link: https://codeforces.com/contest/2126/problem/C
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
