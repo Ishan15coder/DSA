9eafd5bde81b9265e47e11fad65ad7dabbb51b03
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-05 10:02:34 +0530

    Add Codeforces/2126/A/try-1.cpp

---
+++ Codeforces/2126/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Only One Digit
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/17/2025, 8:09:18 PM
+ * Link: https://codeforces.com/contest/2126/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
