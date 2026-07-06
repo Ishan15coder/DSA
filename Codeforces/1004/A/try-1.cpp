1c55791cf1d2d11e69825872eeb68a1c2eb223c1
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:21:19 +0530

    Add Codeforces/1004/A/try-1.cpp

---
+++ Codeforces/1004/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Sonya and Hotels
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/9/2025, 12:38:27 AM
+ * Link: https://codeforces.com/contest/1004/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
