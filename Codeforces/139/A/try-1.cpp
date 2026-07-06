5d4a812612c442113674fae04b5f468890541c7a
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:23:13 +0530

    Add Codeforces/139/A/try-1.cpp

---
+++ Codeforces/139/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Petr and Book
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 6/26/2025, 10:46:44 PM
+ * Link: https://codeforces.com/contest/139/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
