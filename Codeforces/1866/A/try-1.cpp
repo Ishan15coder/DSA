b679b9180a5b94f3d2954b679213905545c371c6
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:22:45 +0530

    Add Codeforces/1866/A/try-1.cpp

---
+++ Codeforces/1866/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Ambitious Kid
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/9/2025, 10:55:01 AM
+ * Link: https://codeforces.com/contest/1866/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
