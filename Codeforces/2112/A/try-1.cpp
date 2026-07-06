f3eb4385a52632317b716d643b0a7b2e09d43496
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:23:28 +0530

    Add Codeforces/2112/A/try-1.cpp

---
+++ Codeforces/2112/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Race
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 6/23/2025, 8:41:04 PM
+ * Link: https://codeforces.com/contest/2112/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
