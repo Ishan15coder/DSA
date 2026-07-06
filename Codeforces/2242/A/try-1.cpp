999d544bb59d7478e73f0fd70a8c1a103f989b80
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:24:09 +0530

    Add Codeforces/2242/A/try-1.cpp

---
+++ Codeforces/2242/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Bigrams
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 7/6/2026, 8:16:15 PM
+ * Link: https://codeforces.com/contest/2242/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
