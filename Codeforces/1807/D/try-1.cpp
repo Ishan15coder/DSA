978b5017bc8876ce8995cb998149e176f29574fb
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:21:48 +0530

    Add Codeforces/1807/D/try-1.cpp

---
+++ Codeforces/1807/D/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Odd Queries
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 8/31/2025, 12:51:33 PM
+ * Link: https://codeforces.com/contest/1807/problem/D
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
