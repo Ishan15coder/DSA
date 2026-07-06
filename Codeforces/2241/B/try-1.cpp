f13201fe843b9dc523c63fe76191915ccbfbf0be
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:17:01 +0530

    Add Codeforces/2241/B/try-1.cpp

---
+++ Codeforces/2241/B/try-1.cpp
@@ -0,0 +1,32 @@
+/*
+ * Problem : Good times Good times
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 6/30/2026, 10:11:47 PM
+ * Link: https://codeforces.com/contest/2241/problem/B
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+
+int main(){
+  ll t;
+  cin>>t;
+  while(t--){
+     ll x;
+     cin>>x;
+        
+     ll cnt=to_string(x).size();
+          ll z=1;
+                for(int i=0;i<cnt;i++){
+        z=z*10;
+     }
+        
+           z++;
+      cout<<z<<endl;
+    }
+}
