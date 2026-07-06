4d56a2e99d978a9015b8065c8b76b330ebbb293b
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:21:34 +0530

    Add Codeforces/296/A/try-1.cpp

---
+++ Codeforces/296/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Yaroslav and Permutations
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/2/2025, 1:42:03 PM
+ * Link: https://codeforces.com/contest/296/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
