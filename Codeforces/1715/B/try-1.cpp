3d533e6793336e2e14b50310cf8a2f59903f7e03
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:18:59 +0530

    Add Codeforces/1715/B/try-1.cpp

---
+++ Codeforces/1715/B/try-1.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem : Beautiful Array
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 2/3/2026, 7:35:45 PM
+ * Link: https://codeforces.com/contest/1715/problem/B
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n,k,b,s;
+        cin>>n>>k>>b>>s;
+        long long mi=k*b;
+        long long ma=(k*b) + (k-1)*n;
+        if(s<mi||s>ma){
+            cout<<-1<<endl;
+        }
+        else{
+            vector<long long>ans(n,0);
+            ans[0]=mi;
+            s-=mi;
+            for(int i=0;i<n;i++){
+                long long add=min(k-1,s);
+                ans[i]+=add;
+                s-=add;
+
+            }
+            for(long long i=0; i<n;i++){
+                cout<<ans[i]<<" ";
+            }
+            cout<<endl;
+        }
+
+
+        
+    }
+
+    return 0;
+}
