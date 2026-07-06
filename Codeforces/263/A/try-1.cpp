2159dc5b2eb957ce440ebeac2e0f530fd1bb1a73
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:23:55 +0530

    Add Codeforces/263/A/try-1.cpp

---
+++ Codeforces/263/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Beautiful Matrix
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 5/29/2025, 10:47:18 AM
+ * Link: https://codeforces.com/contest/263/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
