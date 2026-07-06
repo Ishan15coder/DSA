c8dcdc49f1e66a70f490501da98b3678c4522b34
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:23:13 +0530

    Add Codeforces/224/A/try-1.cpp

---
+++ Codeforces/224/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Parallelepiped
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 6/26/2025, 11:01:31 PM
+ * Link: https://codeforces.com/contest/224/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
