09b863ee0b1a39bbc808d7091ebe22adb3854d40
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:21:04 +0530

    Add Codeforces/791/A/try-1.cpp

---
+++ Codeforces/791/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Bear and Big Brother
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/10/2025, 10:41:30 AM
+ * Link: https://codeforces.com/contest/791/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
