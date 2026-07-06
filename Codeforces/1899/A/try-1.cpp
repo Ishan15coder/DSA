8519bb3aae73338324288eca1f81f1dfad9a5724
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:22:58 +0530

    Add Codeforces/1899/A/try-1.cpp

---
+++ Codeforces/1899/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Game with Integers
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/6/2025, 11:10:57 AM
+ * Link: https://codeforces.com/contest/1899/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
