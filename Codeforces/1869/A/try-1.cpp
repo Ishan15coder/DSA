dd05c19352b38330b8d6822a426a0d48f507777d
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:22:02 +0530

    Add Codeforces/1869/A/try-1.cpp

---
+++ Codeforces/1869/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Make It Zero
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 8/20/2025, 7:58:33 PM
+ * Link: https://codeforces.com/contest/1869/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
