0d407b151be135f091cc57ebf1a6b9bd2c7c8b2e
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:23:42 +0530

    Add Codeforces/118/A/try-1.cpp

---
+++ Codeforces/118/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : String Task
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 6/1/2025, 5:57:16 PM
+ * Link: https://codeforces.com/contest/118/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
