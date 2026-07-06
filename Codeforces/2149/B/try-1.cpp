64a3e10a5eea5a3fb04b590b2270ee8bed300918
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:20:13 +0530

    Add Codeforces/2149/B/try-1.cpp

---
+++ Codeforces/2149/B/try-1.cpp
@@ -0,0 +1,40 @@
+/*
+ * Problem : Unconventional Pairs
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/25/2025, 8:18:37 PM
+ * Link: https://codeforces.com/contest/2149/problem/B
+ */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        int ma=INT_MIN;
+        for(int i=1;i<n;i++){
+            if((i+1)%2==0){
+            ma=max((a[i]-a[i-1]),ma);
+            }           
+        }
+        cout<<ma<<endl;
+    }
+
+
+
+
+    return 0;
+}
