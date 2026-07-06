ce5d34794f07ca1c64b08402eeebb7fa272c0815
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:19:38 +0530

    Add Codeforces/1471/A/try-1.cpp

---
+++ Codeforces/1471/A/try-1.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem : Strange Partition
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 12/30/2025, 12:37:59 AM
+ * Link: https://codeforces.com/contest/1471/problem/A
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        vector<long long>a(n);
+        long long s=0;
+        long long s2=0;
+        for(long long i=0;i<n;i++){
+            cin>>a[i];
+            s+=a[i];
+            s2+=ceil((a[i]*1.0)/x);
+        }
+        s=ceil((s*1.0)/x);
+        cout<<s<<" "<<s2<<endl;
+    }
+
+
+
+    return 0;
+}
