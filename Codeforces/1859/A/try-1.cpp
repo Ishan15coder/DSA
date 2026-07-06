74f1071097d9082443e0e284bb76645d28e9d594
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:22:44 +0530

    Add Codeforces/1859/A/try-1.cpp

---
+++ Codeforces/1859/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : United We Stand
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/11/2025, 11:28:48 AM
+ * Link: https://codeforces.com/contest/1859/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
