df81f641a0b54468eb3a2cb19d631b5b88be24d0
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:20:49 +0530

    Add Codeforces/2148/C/try-1.cpp

---
+++ Codeforces/2148/C/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Pacer
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/13/2025, 9:19:45 PM
+ * Link: https://codeforces.com/contest/2148/problem/C
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
