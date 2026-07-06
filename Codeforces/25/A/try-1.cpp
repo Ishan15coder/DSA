faec10c9180aaf134b55614ee8be942f0e3443bb
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:22:01 +0530

    Add Codeforces/25/A/try-1.cpp

---
+++ Codeforces/25/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : IQ test
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 8/25/2025, 8:29:21 PM
+ * Link: https://codeforces.com/contest/25/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
