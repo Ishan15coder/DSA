f73eee0d17f689cba17ff6e8c4ffc7cfe8110a64
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:22:16 +0530

    Add Codeforces/1883/B/try-1.cpp

---
+++ Codeforces/1883/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Chemistry
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 8/8/2025, 9:19:02 PM
+ * Link: https://codeforces.com/contest/1883/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
