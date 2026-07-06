781deafffd3fec660fcc81a7d8580595e935266e
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:20:36 +0530

    Add Codeforces/2146/A/try-1.cpp

---
+++ Codeforces/2146/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Equal Occurrences
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/21/2025, 8:38:08 PM
+ * Link: https://codeforces.com/contest/2146/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
