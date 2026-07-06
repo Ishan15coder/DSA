5a232512e9b821be174bf63abc4d40cb485a405e
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:22:30 +0530

    Add Codeforces/1904/A/try-1.cpp

---
+++ Codeforces/1904/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Forked!
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 8/7/2025, 7:49:29 PM
+ * Link: https://codeforces.com/contest/1904/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
