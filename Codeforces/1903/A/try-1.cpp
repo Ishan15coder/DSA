b51cf3da54881a4029bf8feb2e51f51f50f8d275
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-06 20:23:56 +0530

    Add Codeforces/1903/A/try-1.cpp

---
+++ Codeforces/1903/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Halloumi Boxes
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 4/20/2025, 11:34:47 AM
+ * Link: https://codeforces.com/contest/1903/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
