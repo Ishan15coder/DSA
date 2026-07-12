908c68979952531f9a19c6d506d06766a54ade14
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-12 20:19:35 +0530

    Add Codeforces/467/A/try-1.cpp

---
+++ Codeforces/467/A/try-1.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem : George and Accommodation
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 5/29/2025, 2:18:21 PM
+ * Link: https://codeforces.com/contest/467/problem/A
+ */
+
+#include<iostream>
+#include<cmath>
+#include<set>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=0;i<n;i++){
+        int p,q;
+        cin>>p>>q;
+        if(q-p>=2)
+        {
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
