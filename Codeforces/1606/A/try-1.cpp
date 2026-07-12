c938a3fe7c4e3fdd8f33a74ed392cae30d432946
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-12 20:18:16 +0530

    Add Codeforces/1606/A/try-1.cpp

---
+++ Codeforces/1606/A/try-1.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem : AB Balance
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/29/2025, 6:38:51 PM
+ * Link: https://codeforces.com/contest/1606/problem/A
+ */
+
+#include<bits/stdc++.h>
+#include<cstring>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s[0]!=s[s.length()-1]){
+            s[0]=s[s.length()-1];
+        }
+        cout<<s<<endl;
+    }
+
+
+    return 0;
+}
