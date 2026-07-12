93d4a06542c91ce9ad37c24b5ab6ac6cc5d4eb98
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-12 20:27:04 +0530

    Add Codeforces/950/A/try-1.cpp

---
+++ Codeforces/950/A/try-1.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem : Left-handers, Right-handers and Ambidexters
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++17 (GCC 7-32)
+ * Date: 9/7/2025, 1:59:13 AM
+ * Link: https://codeforces.com/contest/950/problem/A
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int l,r,a;
+    cin>>l>>r>>a;
+    int ans=0;
+    if(l==r){
+        if(a%2==0){
+            ans=l*2+a;
+        }
+        else{
+            ans=l*2+a-1;
+        }
+    }
+    else{
+        int mi=min(l,r);
+        int ma=max(l,r);
+            if(mi+a==ma)ans=ma*2;
+            else if(mi+a>ma){
+                a=a-(ma-mi);
+                if(a%2==0)ans=ma*2+a;
+                else{
+                    ans=ma*2+a-1;
+                }
+            }
+            else{
+                mi=mi+a;
+                ans=mi*2;
+            }
+    }
+    cout<<ans<<endl;
+    
+    return 0;
+}
