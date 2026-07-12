7e484416dd81eacba9c05cd7c9e56b7663fa9b93
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-12 20:19:33 +0530

    Add Codeforces/69/A/try-1.cpp

---
+++ Codeforces/69/A/try-1.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem : Young Physicist
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 6/1/2025, 6:06:55 PM
+ * Link: https://codeforces.com/contest/69/problem/A
+ */
+
+#include <iostream>
+#include<set>
+#include<string>
+#include <cctype>
+using namespace std;
+int main() {
+   int n;
+   cin>>n;
+   int sumx=0;
+   int sumy=0;
+   int sumz=0;
+   for(int i=0;i<n;i++){
+       int a,b,c;
+       cin>>a>>b>>c;
+       sumx+=a;
+       sumy+=b;
+       sumz+=c;
+   }
+   if(sumx==0&&sumy==0&&sumz==0){
+       cout<<"YES";
+   }
+   else{
+     cout<<"NO"; 
+   }
+    return 0;
+}
