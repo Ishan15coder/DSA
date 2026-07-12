1e81830b9927aba75ce065aa759a2d38175da54e
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-12 20:19:16 +0530

    Add Codeforces/1877/A/try-1.cpp

---
+++ Codeforces/1877/A/try-1.cpp
@@ -0,0 +1,32 @@
+/*
+ * Problem : Goals of Victory
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 7/6/2025, 10:49:49 PM
+ * Link: https://codeforces.com/contest/1877/problem/A
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t>0){
+        int n;
+        cin>>n;
+        int s=0;
+        for(int i=0;i<n-1;i++){
+            int p;
+            cin>>p;
+            s+=p;
+        }
+        cout<<-(s)<<endl;;
+
+        t--;
+    }
+
+
+    return 0;
+}
