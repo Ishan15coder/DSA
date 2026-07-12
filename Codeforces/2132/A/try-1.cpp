c2f7f0356baec9023a8a4416f204902ac816c0a8
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-12 20:19:02 +0530

    Add Codeforces/2132/A/try-1.cpp

---
+++ Codeforces/2132/A/try-1.cpp
@@ -0,0 +1,42 @@
+/*
+ * Problem : Homework
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 8/21/2025, 8:14:07 PM
+ * Link: https://codeforces.com/contest/2132/problem/A
+ */
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string a;
+        cin>>a;
+        int m;
+        cin>>m;
+        string b;
+        cin>>b;
+        string c;
+        cin>>c;
+        string ans="";
+        for(int i=0;i<m;i++){
+            if(c[i]=='D'){
+                a+=b[i];
+            }
+            else{
+                ans="";
+                ans+=b[i];
+                ans+=a;
+                a=ans;
+            }
+        }
+        cout<<a<<endl;
+    }
+}
