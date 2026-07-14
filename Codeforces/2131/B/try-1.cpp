863bd731252708d22f83c3e18d0ebd22fd548738
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-14 21:32:08 +0530

    Add Codeforces/2131/B/try-1.cpp

---
+++ Codeforces/2131/B/try-1.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem : Alternating Series
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 8/10/2025, 9:21:47 PM
+ * Link: https://codeforces.com/contest/2131/problem/B
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) cout << -1 << " ";
+            else if(n%2==0&&i==n)cout<<2<<" ";
+            else cout<<3<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+    return 0;
+}
