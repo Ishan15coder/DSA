d90fed691ede201821c9268a645253742173deb9
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-14 20:54:43 +0530

    Add Codeforces/1679/A/try-1.cpp

---
+++ Codeforces/1679/A/try-1.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem : AvtoBus
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++17 (GCC 7-32)
+ * Date: 9/6/2025, 4:35:22 PM
+ * Link: https://codeforces.com/contest/1679/problem/A
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        long long a=-1,b=-1;
+        if(n<4||n%2!=0)cout<<-1<<endl;
+        else{
+            a=ceil(n*1.0/6);
+            b=n/4;
+            cout<<a<<" "<<b<<endl;
+        }
+    }
+    return 0;
+}
