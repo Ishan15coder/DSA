dc3d0d61faf043cdce1c256c7ce5293f4ce788ce
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-07-16 20:34:33 +0530

    Add Codeforces/282/A/try-1.cpp

---
+++ Codeforces/282/A/try-1.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem : Bit++
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++20 (GCC 13-64)
+ * Date: 4/6/2025, 7:52:28 PM
+ * Link: https://codeforces.com/contest/282/problem/A
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x=0;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        int p=s.length();
+        for(int i=0;i<p;i++){
+            if(s[i]=='+'&&s[i+1]=='+'){
+                x++;
+                break;
+            }
+            if(s[i]=='-'&&s[i+1]=='-'){
+                x--;
+                break;
+            }
+        }
+    }
+    cout<<x;
+
+
+
+    return 0;
+}
