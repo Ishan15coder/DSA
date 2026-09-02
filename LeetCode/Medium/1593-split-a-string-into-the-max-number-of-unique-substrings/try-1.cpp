b8d4e1059cb2a01584865711d09e8c15b2f03aac
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-02 21:47:35 +0530

    Add LeetCode/Medium/1593-split-a-string-into-the-max-number-of-unique-substrings/try-1.cpp

---
+++ LeetCode/Medium/1593-split-a-string-into-the-max-number-of-unique-substrings/try-1.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem #1593: Split a String Into the Max Number of Unique Substrings
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 9/1/2026, 4:56:51 PM
+ * Link: https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/
+ */
+
+class Solution {
+public:
+    set<vector<string>>ans;
+    vector<string>se;
+    void gen(string s,int idx){
+        if(idx==s.length()){
+            // set<string>st;
+            // for(auto it:se){
+            //     st.insert(it);
+            // }
+            ans.insert(se);
+            return;
+        }
+        string t="";
+        for(int i=idx;i<s.length();i++){
+            t+=s[i];
+            int ch=0;
+            if(find(se.begin(), se.end(), t)==se.end()){
+                se.push_back(t);
+                ch=1;
+                }
+            gen(s,i+1);
+            if(ch)se.pop_back();
+        }
+    }
+    int maxUniqueSplit(string s) {
+        gen(s,0);
+        int ma=0;
+        for(auto it:ans){
+            int k=it.size();
+            ma=max(ma,k);
+        }
+        return ma;
+    }
+};
