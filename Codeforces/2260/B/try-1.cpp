e5e75ab3673a218aeb0154ba0a73d38c3b492d31
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-08 21:13:53 +0530

    Add Codeforces/2260/B/try-1.cpp

---
+++ Codeforces/2260/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Monocarp and Projects
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/8/2026, 9:12:54 PM
+ * Link: https://codeforces.com/contest/2260/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
