2aaafa3bfaa711359efc27cd82032fbd56b9a947
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-09 19:35:10 +0530

    Add Codeforces/1729/D/try-1.cpp

---
+++ Codeforces/1729/D/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Friends and the Restaurant
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/9/2026, 7:29:37 PM
+ * Link: https://codeforces.com/contest/1729/problem/D
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
