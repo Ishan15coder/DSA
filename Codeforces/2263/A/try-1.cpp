9ee7a05a2baf2fac65f3fa0cf5d2a5e16cfd94e4
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-12 20:10:35 +0530

    Add Codeforces/2263/A/try-1.cpp

---
+++ Codeforces/2263/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Min Max Game
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/12/2026, 8:09:53 PM
+ * Link: https://codeforces.com/contest/2263/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
