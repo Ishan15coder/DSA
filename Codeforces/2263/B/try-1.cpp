7def941ab055d6140895b41a1bd220b2bb5f9418
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-12 20:47:26 +0530

    Add Codeforces/2263/B/try-1.cpp

---
+++ Codeforces/2263/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Min Matrices
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/12/2026, 8:46:48 PM
+ * Link: https://codeforces.com/contest/2263/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
