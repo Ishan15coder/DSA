b1d7ddbd1d5a2dd71894df046ab8f43c82cb602f
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-13 22:48:45 +0530

    Add Codeforces/2264/A/try-1.cpp

---
+++ Codeforces/2264/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Rumb Needs a Hand
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/13/2026, 10:48:07 PM
+ * Link: https://codeforces.com/contest/2264/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
