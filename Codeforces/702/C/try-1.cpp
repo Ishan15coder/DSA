7255525ecfb4de2aa4bb38106195542c1dbfc24f
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-15 19:56:18 +0530

    Add Codeforces/702/C/try-1.cpp

---
+++ Codeforces/702/C/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Cellular Network
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/15/2026, 7:55:52 PM
+ * Link: https://codeforces.com/contest/702/problem/C
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
