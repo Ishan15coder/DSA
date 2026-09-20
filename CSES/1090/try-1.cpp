e1ad2462a08474fad52c21dbb6f78227aa570a91
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-20 19:45:53 +0530

    Add CSES/1090/try-1.cpp

---
+++ CSES/1090/try-1.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem : CSES Task 1090
+ * Difficulty: Unknown
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++
+ * Date: 9/20/2026, 7:45:50 PM
+ * Link: https://cses.fi/problemset/task/1090
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:12:15: warning: unused variable 'c' [-Wunused-variable]
+   12 |     long long c=0;
+      |               ^
