64b810552b40ad67ca2044c810eb25ea636036ce
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-20 15:53:02 +0530

    Add Codeforces/1201/C/try-1.cpp

---
+++ Codeforces/1201/C/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Maximum Median
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/20/2026, 3:52:39 PM
+ * Link: https://codeforces.com/contest/1201/problem/C
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
