a9967402238968c6463319483ee5d60574a69b45
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-21 20:10:33 +0530

    Add Codeforces/2266/A/try-1.cpp

---
+++ Codeforces/2266/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Good Contest
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/21/2026, 8:09:41 PM
+ * Link: https://codeforces.com/contest/2266/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
