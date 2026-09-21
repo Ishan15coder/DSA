b486dba824c9929d3919baba07bf7cdf9ecb2230
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-21 21:00:56 +0530

    Add Codeforces/2266/C/try-1.cpp

---
+++ Codeforces/2266/C/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : AND, OR, Sort!
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/21/2026, 9:00:22 PM
+ * Link: https://codeforces.com/contest/2266/problem/C
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
