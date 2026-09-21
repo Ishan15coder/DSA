e5860767b7de8830f92ba547550a7dd4c6d8a145
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-21 20:17:38 +0530

    Add Codeforces/2266/B/try-1.cpp

---
+++ Codeforces/2266/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Three Piles
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/21/2026, 8:17:01 PM
+ * Link: https://codeforces.com/contest/2266/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
