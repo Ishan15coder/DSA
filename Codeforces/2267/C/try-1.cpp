038de2e03c9c6c2fdb95e41e17cbba850a734d33
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-25 22:05:54 +0530

    Add Codeforces/2267/C/try-1.cpp

---
+++ Codeforces/2267/C/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : GCD Treasury
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/25/2026, 10:04:39 PM
+ * Link: https://codeforces.com/contest/2267/problem/C
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
