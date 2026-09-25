8f171d04cdfa217fe6bb0e3ff967aa0cb963b376
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-25 20:21:53 +0530

    Add Codeforces/2267/B/try-1.cpp

---
+++ Codeforces/2267/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Fashionable Array
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/25/2026, 8:21:28 PM
+ * Link: https://codeforces.com/contest/2267/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
