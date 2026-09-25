14a02950e03bf92881b644bb256429f8d3cb5067
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-25 20:10:35 +0530

    Add Codeforces/2267/A/try-1.cpp

---
+++ Codeforces/2267/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : Turn Into a Palindrome
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/25/2026, 8:10:01 PM
+ * Link: https://codeforces.com/contest/2267/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
