50ece748cd6138083bf791bd1d088553ef688ebf
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-26 20:13:40 +0530

    Add Codeforces/2269/A/try-1.cpp

---
+++ Codeforces/2269/A/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : SauSaGe Bank
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/26/2026, 8:13:10 PM
+ * Link: https://codeforces.com/contest/2269/problem/A
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
