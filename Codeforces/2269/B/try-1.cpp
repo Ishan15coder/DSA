e63cb12c7e2bcbf1ad1bd771e959f26d49d91134
Ishan15coder <100475440+Ishan15coder@users.noreply.github.com>
2026-09-26 21:27:37 +0530

    Add Codeforces/2269/B/try-1.cpp

---
+++ Codeforces/2269/B/try-1.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem : KiaKio and Squared Numbers
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: C++23 (GCC 14-64, msys2)
+ * Date: 9/26/2026, 9:26:34 PM
+ * Link: https://codeforces.com/contest/2269/problem/B
+ */
+
+// Codeforces Private Group Submission
+// Code is not publicly accessible.
